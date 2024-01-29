d8ebc538eadaa851f969f06bac475141614a32f1
Nick <nicholas.coppola21@outlook.com>
2024-01-28 20:44:17 -0600

    fix(0.7.1): Export all parsers

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,7 @@ export { default as SkywardClass } from "./SkywardClass.js";
 export * from "./SkywardClass.js";
 export * from "./GradeBookManager.js";
 export * from "./SkywardAccountManager.js";
+export * from "./parsers/AttendanceParser.js";
+export * from "./parsers/ReportCardParser.js";
 // ex final link: https://skyward-ccisdprod.iscorp.com/scripts/wsisa.dll/WService=wseduclearcreektx/qdirconfig002.p?destinationPage=0x0000000002d43d81&asAttach=no&dwd=255331&pEnc=dcidIclhjskcRlHc&duserid=100037965%40ccisd.net&Browser=Firefox&BrowserPlatform=MacIntel
 // format destination as  https://skyward-ccisdprod.iscorp.com/scripts/wsisa.dll/WService=wseduclearcreektx/{dest formatted}
